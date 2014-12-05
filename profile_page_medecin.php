a1d6dc0260cfe2d207ba4ebb6f84e1f2fa085ab9
James Batten <james.batten@epita.fr>
2014-12-05 06:36:31 +0100

    added medecin profile page

---
+++ profile_page_medecin.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>HumaneWeb Admin</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet">
+	</head>
+	<body>
+<!-- Header -->
+<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">HumaneWeb</a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown">
+          <ul id="g-account-menu" class="dropdown-menu" role="menu">
+            <li><a href="#">My Profile</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Logout</a></li>
+      </ul>
+    </div>
+  </div><!-- /container -->
+</div>
+<!-- /Header -->
+
+<!-- Main -->
+<div class="container-fluid">
+<div class="row">
+	<div class="col-sm-3">
+      <!-- Left column -->
+      <a href="#"><strong><i class="glyphicon glyphicon-wrench"></i> Tools</strong></a>  
+      
+      <hr>
+      
+      <ul class="list-unstyled">
+        <li class="nav-header"> <a href="#" data-toggle="collapse" data-target="#userMenu">
+          <h5>Settings <i class="glyphicon glyphicon-chevron-down"></i></h5>
+          </a>
+            <ul class="list-unstyled collapse in" id="userMenu">
+                <li class="active"> <a href="#"><i class="glyphicon glyphicon-home"></i> Home</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-envelope"></i> Messages <span class="badge badge-info">4</span></a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-cog"></i> Options</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-off"></i> Logout</a></li>
+            </ul>
+        </li>
+      </ul>
+           
+      
+      
+  	</div><!-- /col-3 -->
+    <div class="col-sm-9">
+      
+		<div class="row">
+           
+            
+          
+            <!-- center left-->	
+              
+			  <!--tabs-->
+              <div class="container">
+                <div class="col-md-4">
+                <ul class="nav nav-tabs" id="myTab">
+                  <li class="active"><a href="#profile" data-toggle="tab">Profile</a></li>
+                  <li><a href="#messages" data-toggle="tab">Messages</a></li>
+                  <li><a href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+                
+                <div class="tab-content">
+                  <div class="tab-pane active" id="profile">
+                    <h4><i class="glyphicon glyphicon-user"></i></h4>
+                  </div>
+                  <div class="tab-pane" id="messages">
+                    <h4><i class="glyphicon glyphicon-comment"></i></h4>
+                  </div>
+                  <div class="tab-pane" id="settings">
+                    <h4><i class="glyphicon glyphicon-cog"></i></h4>
+                  </div>
+                </div>
+              	</div>
+              </div>  
+               
+              <!--/tabs-->
+              
+          	</div><!--/col-->
+              
+<hr>
+
+     <h3>Search for person by name</h3> 
+      <form  method="post" action="search.php?go"  id="searchform"> 
+        <input  type="text" name="name"> 
+        <input  type="submit" name="submit" value="Search"> 
+      </form> 
+			</div><!--/col-span-6-->
+     
+      </div><!--/row-->
+      
+      
+  	</div><!--/col-span-9-->
+</div>
+</div>
+
+<!-- /Main -->
+
+  
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
