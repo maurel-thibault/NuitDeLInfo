3cae35f4e1f937987aa47f347fec363ca806b5a1
Aurélien Bareille <aurelien.bareille@epita.fr>
2014-12-05 07:47:37 +0100

    New sql base

--- nuit_info.sql
+++ nuit_info.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 05 Décembre 2014 à 06:11
+-- Généré le :  Ven 05 Décembre 2014 à 07:46
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `refugee` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lastname` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `origin_city` varchar(255) NOT NULL,
   `marital_status` int(11) NOT NULL,
   `children` int(11) NOT NULL,
@@ -37,15 +38,14 @@ CREATE TABLE IF NOT EXISTS `refugee` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Contenu de la table `refugee`
 --
 
-INSERT INTO `refugee` (`id`, `lastname`, `surname`, `origin_city`, `marital_status`, `children`, `pic`) VALUES
-(8, '', 'AUR', 'plop', 0, 0, ''),
-(9, '', 'AUR', 'plop', 0, 0, '');
+INSERT INTO `refugee` (`id`, `lastname`, `surname`, `pass`, `origin_city`, `marital_status`, `children`, `pic`) VALUES
+(12, 'BAR', 'AUR', 'griIsZM8FH43I', 'plop', 0, 0, '');
 
 -- --------------------------------------------------------
 
@@ -65,7 +65,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `id_2` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `pseudo`, `pass`, `email`, `date_inscription`, `lastname`, `surname`, `status`) VALUES
+(3, 'plop', 'griIsZM8FH43I', 'az@az.fr', '2014-12-05', 'BAR', 'AUR', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
